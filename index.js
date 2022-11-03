add9fffd9c449a200820c00566fda3000ec5f9e2
Thomas Vu <thomasjvu@gmail.com>
2022-11-03 16:11:56 -0400

    Initialize project

---
+++ index.js
@@ -0,0 +1,67 @@
+// selects canvas and sets it equal to const canvas
+const canvas = document.querySelector('canvas')
+
+const c = canvas.getContext('2d')
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Player {
+    constructor() {
+
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+
+        const image = new Image()
+        image.src = './img/shipFullHealth.png'
+        image.onload = () => {
+            const scale = 2
+            this.image =  image
+            this.width = image.width * scale
+            this.height = image.height * scale
+
+
+        this.position = {
+            x: canvas.width / 2 - this.width / 2,
+            y: canvas.height / 2 - this.height / 2
+        }
+
+        }
+
+    }
+
+
+    draw() {
+        // c.fillStyle = 'red'
+        // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        if(this.image)
+            c.drawImage(
+                this.image, 
+                this.position.x, 
+                this.position.y,
+                this.width,
+                this.height
+            )
+        }
+        
+}
+
+const player = new Player()
+player.draw()
+
+function animate() {
+    requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.draw()
+    console.log('animate log')
+}
+
+animate()
+
+addEventListener('keydown', (event) => {
+ console.log(event.key)
+})
+
+
